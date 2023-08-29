b38e9a4e781b9121f2c46eba61194f97de73e128
sun0lee <67420397+sun0lee@users.noreply.github.com>
2023-08-30 00:46:53 +0900

    font적용하기
    - theme : aaa , font 추가
    - githib-markdown.css > body font 적용

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,14 +12,14 @@ export default class MyDocument extends Document {
         return { ...initialProps };
     }
 
-    // testtt 
+    // aaa
     // lara-light-indigo
     // soho-light
     render() {
         return (
             <Html lang="en">
                 <Head>
-                    <link id="theme-css" href={`/themes/testtt/theme.css`} rel="stylesheet"></link>
+                    <link id="theme-css" href={`/themes/aaa/theme.css`} rel="stylesheet"></link>
                 </Head>
                 <body>
                     <Main />
